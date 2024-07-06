db2a6e60909b2ba1fe55dd637095d310fef89d00
Ali Hashemi <seyedALIhash1@gmail.com>
2024-07-06 15:55:40 +0330

    Django project

---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'user registered successfully', 'success')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged successfully', 'success')
+                return redirect('home')
+            else:
+                messages.error(request, 'username or password is wrong', 'danger')
+                return redirect('home')
+    else:
+        form = UserLoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'logged out successfully')
+    return redirect('home')
---
+++ home/apps.py
@@ -0,0 +1,3 @@
+ascii_code = 65
+char = chr(ascii_code)
+print(f"the character corresponding to {ascii_code} is equal to: {char}")
